a756a48302451b0e5d4261ca3d36b915557b5d0e
Tareq Hasan <tareq1988@gmail.com>
2020-11-13 18:56:07 +0800

    Added 7.4 and other configs

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker build --build-arg PHP_VERSION=7.3 -t tareq1988/wordpress:7.3 -f  wordpress/Dockerfile wordpress
+docker build --build-arg PHP_VERSION=7.4 -t tareq1988/wordpress:7.4 -f  wordpress/Dockerfile wordpress
---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker push tareq1988/wordpress:7.4
+docker push tareq1988/wordpress:7.3
