f5914fa2f1e07f89c046dc362b3e74b074e9abfb
Tareq Hasan <tareq1988@gmail.com>
2024-02-01 18:48:04 +0600

    Add support for PHP 8.3 in build and deploy scripts

--- build.sh
+++ build.sh
@@ -4,3 +4,4 @@ docker build --platform linux/amd64 --build-arg PHP_VERSION=7.4 -t meghsh/php:7.
 docker build --platform linux/amd64 --build-arg PHP_VERSION=8.0 -t meghsh/php:8.0 -f php/Dockerfile php
 docker build --platform linux/amd64 --build-arg PHP_VERSION=8.1 -t meghsh/php:8.1 -f php/Dockerfile php
 docker build --platform linux/amd64 --build-arg PHP_VERSION=8.2 -t meghsh/php:8.2 -f php/Dockerfile php
+docker build --platform linux/amd64 --build-arg PHP_VERSION=8.3 -t meghsh/php:8.3 -f php/Dockerfile php
