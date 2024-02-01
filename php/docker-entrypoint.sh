7ba5d918adbaeff1b5b15860948dbb1e5e08df1f
Tareq Hasan <tareq1988@gmail.com>
2024-02-01 18:47:46 +0600

    feat: configure sendmail_path and fix permissions for /etc/msmtprc file

--- php/docker-entrypoint.sh
+++ php/docker-entrypoint.sh
@@ -2,6 +2,13 @@
 
 set -e
 
+# Check if /etc/msmtprc exists and set permissions
+if [ -f /etc/msmtprc ]; then
+    echo "Configuring /etc/msmtprc permissions..."
+    chown www-data:www-data /etc/msmtprc
+    chmod 600 /etc/msmtprc
+fi
+
 # first arg is `-f` or `--some-option`
 if [ "${1#-}" != "$1" ]; then
     set -- php-fpm "$@"
