adb4e2f6f8deef5d8a60edcdbbff3ad1106f7b4f
Tareq Hasan <tareq1988@gmail.com>
2024-03-06 23:09:33 +0600

    fix: Remove unnecessary code for configuring /etc/msmtprc permissions

--- php/docker-entrypoint.sh
+++ php/docker-entrypoint.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-set -e
-
-# Check if /etc/msmtprc exists and set permissions
-if [ -f /etc/msmtprc ]; then
-    echo "Configuring /etc/msmtprc permissions..."
-    chown www-data:www-data /etc/msmtprc
-    chmod 600 /etc/msmtprc
-fi
-
 # first arg is `-f` or `--some-option`
 if [ "${1#-}" != "$1" ]; then
     set -- php-fpm "$@"
